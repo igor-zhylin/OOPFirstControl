f102b41420b0576cdd9db9430ef882823d01befb
igor-zhylin <37139257+igor-zhylin@users.noreply.github.com>
2018-03-08 20:14:57 +0200

    First Commit

---
+++ FileMetaData/Image.cs
@@ -0,0 +1,6 @@
+﻿namespace FileMetadata
+{
+    class Image
+    {
+    }
+}
---
+++ OOPFirstControl/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPFirstControl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String text = @"Text: file.txt(6B); Some string content
+                            Image: img.bmp(19MB); 1920х1080
+                            Text:data.txt(12B); Another string
+                            Text:data1.txt(7B); Yet another string
+                            Movie:logan.2017.mkv(19GB); 1920х1080; 2h12m";
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
