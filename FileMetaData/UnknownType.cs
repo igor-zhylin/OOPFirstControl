97bc4344885e70f8f843e44852f310d353800551
igor-zhylin <37139257+igor-zhylin@users.noreply.github.com>
2018-03-09 01:09:57 +0200

    Wanna Sleep
    Wanna Sleeep
    neeeeed Commit

--- FileMetaData/UnknownType.cs
+++ FileMetaData/UnknownType.cs
@@ -6,8 +6,19 @@
     using System.Text;
     using System.Threading.Tasks;
 
-    public class UnknownType<T> where T : MetaData
+    public class UnknownType : MetaData
     {
 
+        protected string[] UnknownAtributes { get; set; }
+
+        public UnknownType(
+            string Name,
+            string Format,
+            string Size,
+            string[] Atributes)
+            : base(Name, Format, Size, Atributes)
+        {
+            this.UnknownAtributes = Atributes;
+        }
     }
 }
--- Parser/Parse.cs
+++ Parser/Parse.cs
@@ -1,18 +1,68 @@
 ﻿
 namespace Parser
 {
-    using System;
     using System.Collections.Generic;
+    using FileMetaData;
     using System.Linq;
     using System.Text;
     using System.Threading.Tasks;
 
     public class Parse
     {
+        #region Separators
+
         private const string ExtentionSeparator = ".";
         private const string FileTypeSeparator = ":";
+        private const string Separator = ";";
+
+        #endregion
+
+        private string[] _fileEntities;
+        private string _input;
+        private List<MetaData> metaData;
+
+        #region Costructors
+
+        public Parse()
+        {
+            metaData = new List<MetaData>();
+        }
+
+        public Parse(string input)
+        {
+            this._input = input;
+            metaData = new List<MetaData>();
+        }
+
+        #endregion
+
+        public void SetInputString(string input)
+        {
+            this._input = input;
+        }
+
+        public List<MetaData> RunParser()
+        {
 
 
 
+            return metaData;
+        }
+
+        private MetaData CreateNewFileMetaData(
+            string Name,
+            string Format,
+            string Size,
+            string[] Atributes)
+        {
+            switch (Format.ToLower())
+            {
+                case "text": return new Text(Name, Format, Size, Atributes);
+                case "movie": return new Movie(Name, Format, Size, Atributes);
+                case "image": return new Image(Name, Format, Size, Atributes);
+                default:
+                    return new UnknownType(Name, Format, Size, Atributes);
+            }
+        }
     }
 }
